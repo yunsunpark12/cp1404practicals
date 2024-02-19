920806d3b426d04d9732f46f0a6915ec43b57c44
yunsun <ys247071@gmail.com>
2024-02-19 16:57:44 +0800

    Add menu, message deciding function, and star showing function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,6 +2,7 @@ MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 END_SCRIPT = "Finished"
 ERROR_SCRIPT = "Invalid Input"
 ERROR_SCRIPT_NUMBER = "Score should be 0-100 inclusive."
+ERROR_SCRIPT_INTEGAR = "Invalid input, not an integer"
 SCORE_SCRIPT_90 = "Excellent"
 SCORE_SCRIPT_50 = "Passable"
 SCORE_SCRIPT_BAD = "bad"
@@ -12,13 +13,16 @@ def main():
     choice = input("Enter choice: ").upper()
 
     while choice != "Q":
-
         if choice == "G":
-            score = int(input("Enter score: "))
-            while score < 0 or score > 100:
-                print(ERROR_SCRIPT_NUMBER)
-                score = float(input("Enter score: "))
-            decide_message(score)
+            while score is True:
+                try:
+                    score = int(input("Enter score: "))
+                    while score < 0 or score > 100:
+                        print(ERROR_SCRIPT_NUMBER)
+                        score = float(input("Enter score: "))
+                    decide_message(score)
+                except ValueError:
+                    print(ERROR_SCRIPT_INTEGAR)
         elif choice == "P":
             result_message = decide_message(score)
             print(result_message)
