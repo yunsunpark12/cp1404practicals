c0d8b9ae09fd4e85934e8394165e89140ee9e758
yunsun <ys247071@gmail.com>
2024-02-19 17:42:00 +0800

    Add docstrings

--- prac_02/extension_work/more_scores.py
+++ prac_02/extension_work/more_scores.py
@@ -8,16 +8,18 @@ END_SCRIPT = "Finished"
 
 def main():
     score_no = int(input("Enter number of scores: "))
-    while score_no < 0:
+    while score_no < 0:         # error check
         print(ERROR_SCRIPT)
         score_no = int(input("Enter number of scores: "))
+
+    # choose out random scores as much as input number
     for i in range(score_no):
         random_score = random.randint(0, 101)
         result_message = decide_message(random_score)
         print(f"{random_score} is {result_message}")
     print(END_SCRIPT)
 
-
+# show appropriate message for random score
 def decide_message(random_score):
     if random_score >= 90:
         result_message = SCORE_SCRIPT_90
