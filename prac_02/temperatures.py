894f00d74726e8f495c239aaaccf7574ae5115d8
yunsun <ys247071@gmail.com>
2024-02-19 13:16:32 +0800

    Add calculating functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,25 +1,40 @@
-MENU = """C - Convert Celsius to Fahrenheit
-    F - Convert Fahrenheit to Celsius
-    Q - Quit"""
+MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"
 ERROR_SCRIPT = "Invalid option"
+END_SCRIPT = "Thank you."
 
 def main():
     print(MENU)
+    get_choice()
+    print(END_SCRIPT)
+
+
+def get_choice():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = calculate_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
 
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = calculate_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} degrees C")
 
         else:
             print(ERROR_SCRIPT)
         print(MENU)
         choice = input(">>> ").upper()
-    print("Thank you.")
+
+
+def calculate_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def calculate_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
 main()
