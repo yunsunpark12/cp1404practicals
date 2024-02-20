30ccbd8b9aa9f2dbd0cf6ce6b7e12f627b4791e5
yunsun <ys247071@gmail.com>
2024-02-20 14:30:39 +0800

    Refactor get score function

--- prac_02/extension_work/more_scores.py
+++ prac_02/extension_work/more_scores.py
@@ -1,16 +1,14 @@
 import random
 
 ERROR_SCRIPT = "Invalid input"
-SCORE_SCRIPT_90 = "Excellent"
-SCORE_SCRIPT_50 = "Passable"
-SCORE_SCRIPT_BAD = "Bad"
 END_SCRIPT = "Finished"
+INVALID_SCORE_MINIMUM = 0
+EXCELLENT_SCORE = 90
+PASSABLE_SCORE = 50
 
 def main():
-    score_no = int(input("Enter number of scores: "))
-    while score_no < 0:         # error check
-        print(ERROR_SCRIPT)
-        score_no = int(input("Enter number of scores: "))
+    """get score number, choose random scores, and print results of it"""
+    score_no = get_score()
 
     # choose out random scores as much as input number
     for i in range(score_no):
@@ -19,14 +17,24 @@ def main():
         print(f"{random_score} is {result_message}")
     print(END_SCRIPT)
 
-# show appropriate message for random score
+
+def get_score():
+    """get score and validate"""
+    score_no = int(input("Enter number of scores: "))
+    while score_no < INVALID_SCORE_MINIMUM:
+        print(ERROR_SCRIPT)
+        score_no = int(input("Enter number of scores: "))
+    return score_no
+
+
 def decide_message(random_score):
-    if random_score >= 90:
-        result_message = SCORE_SCRIPT_90
-    elif random_score >= 50:
-        result_message = SCORE_SCRIPT_50
+    """show appropriate message for random score"""
+    if random_score >= EXCELLENT_SCORE:
+        result_message = "Excellent"
+    elif random_score >= PASSABLE_SCORE:
+        result_message = "Passable"
     else:
-        result_message = SCORE_SCRIPT_BAD
+        result_message = "Bad"
     return result_message
 
 
