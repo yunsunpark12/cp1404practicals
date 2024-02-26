94a47f76925e0425e58c56256d8fdde24a0cef7a
yunsun <ys247071@gmail.com>
2024-02-26 14:21:56 +0800

    Add main function

--- prac_03/3_capitalist_conrad.py
+++ prac_03/3_capitalist_conrad.py
@@ -15,21 +15,25 @@ MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
-price = INITIAL_PRICE
-print(f"${price:,.2f}")
 
-while MIN_PRICE <= price <= MAX_PRICE:
-    price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
-        price_change = random.uniform(0, MAX_INCREASE)
-    else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
-
-    price *= (1 + price_change)
+def main():
+    price = INITIAL_PRICE
     print(f"${price:,.2f}")
+    while MIN_PRICE <= price <= MAX_PRICE:
+        price_change = 0
+        # generate a random integer of 1 or 2
+        # if it's 1, the price increases, otherwise it decreases
+        if random.randint(1, 2) == 1:
+            # generate a random floating-point number
+            # between 0 and MAX_INCREASE
+            price_change = random.uniform(0, MAX_INCREASE)
+        else:
+            # generate a random floating-point number
+            # between negative MAX_DECREASE and 0
+            price_change = random.uniform(-MAX_DECREASE, 0)
+
+        price *= (1 + price_change)
+        print(f"${price:,.2f}")
+
+
+main()
