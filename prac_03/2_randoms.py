fb5c3d099ce848df74ae2d0d270c4f32777444d2
yunsun <ys247071@gmail.com>
2024-02-26 14:07:38 +0800

    Answered questions

---
+++ prac_03/2_randoms.py
@@ -0,0 +1,24 @@
+"""
+q1. What did you see on line 1?
+    6
+
+q2. What was the smallest number you could have seen, what was the largest?
+    smallest : 5, largest : 20
+
+q3. What did you see on line 2?
+    5
+
+q4. What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?
+    smallest : 3, largest : 9, no
+
+q5. What did you see on line 3?
+    2.797172500794854
+
+q6. What was the smallest number you could have seen, what was the largest?
+    smallest : 2.797172500794854, largest : 5.419939191095202
+"""
+
+# q7. Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+print(random.randint(1,100))
