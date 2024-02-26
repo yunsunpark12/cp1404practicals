7372d6339b8806e0471795266498b0ebd2bc0dfa
yunsun <ys247071@gmail.com>
2024-02-26 15:30:15 +0800

    Answer Q1,2,3,4

--- prac_03/6_files.py
+++ prac_03/6_files.py
@@ -4,8 +4,51 @@ then opens a file called "6_name.txt" and writes that name to it.
 Remember to close your file.
 """
 
-FILENAME = "6_name.txt"
-out_file = open(FILENAME, 'w')
-name = input("What is your name? ")
-print(name, file = out_file)
-out_file.close()
+# FILENAME = "6_name.txt"
+# out_file = open(FILENAME, 'w')
+# name = input("What is your name? ")
+# print(name, file = out_file)
+# out_file.close()
+
+"""
+Q2. (In the same file, but as if it were a separate program)
+Write code that opens "name.txt" and reads the name (as above) then prints,
+"Your name is Bob" (or whatever the name is in the file).
+"""
+
+# FILENAME = "6_name.txt"
+# in_file = open(FILENAME, 'r')
+# name = in_file.read().strip()
+# print("Your name is", name)
+# in_file.close()
+
+"""
+Q3. Create a text file called numbers.txt and save it in your prac directory. 
+Put the following three numbers on separate lines in the file and save it:
+17
+42
+400
+Write code that opens "numbers.txt", reads only the first two numbers 
+and adds them together then prints the result, which should be... 59.
+"""
+
+# FILENAME = "6_numbers.txt"
+# in_file = open(FILENAME, 'r')
+# number_1 = int(in_file.readline())
+# number_2 = int(in_file.readline())
+# in_file.close()
+# print("Result: ", number_1 + number_2)
+
+"""
+Q4. Now write a fourth block of code that prints 
+the total for all lines in numbers.txt or a file with any number of numbers.
+"""
+
+FILENAME = "6_numbers.txt"
+total = 0
+in_file = open(FILENAME, 'r')
+for line in in_file:
+    number = int(line)
+    total += number
+in_file.close()
+print("Result: ", total)
