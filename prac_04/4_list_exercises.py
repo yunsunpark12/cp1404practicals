5a5f0ad880d353fe76e8f06d21a629b4cb5326c8
yunsun <ys247071@gmail.com>
2024-03-10 18:46:57 +0800

    download code

---
+++ prac_04/4_list_exercises.py
@@ -0,0 +1,2 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
