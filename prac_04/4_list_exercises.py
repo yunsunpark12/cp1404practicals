e4f7582b5356f185d2cc236222f0e5a1e68b6350
yunsun <ys247071@gmail.com>
2024-03-10 18:49:13 +0800

    complete username check feature

--- prac_04/4_list_exercises.py
+++ prac_04/4_list_exercises.py
@@ -1,2 +1,8 @@
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
