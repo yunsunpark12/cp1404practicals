8b44e82423218ce5c720790d0fdee1395732d93e
yunsun <ys247071@gmail.com>
2024-03-11 15:21:27 +0800

    main fuc

--- prac_05/4_emails.py
+++ prac_05/4_emails.py
@@ -3,15 +3,19 @@ Estimate: 40 minutes
 Actual:    minutes
 """
 
-email_to_name = {}
-email = input("Email: ")
-while email != '':
 
-    user_answer = input(f"Is your name {name}? (Y/n)").lower()
-    if user_answer == "n":
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != '':
+
+        user_answer = input(f"Is your name {name}? (Y/n)").lower()
+        if user_answer == "n":
         # get rid of number
-    else:
-        # display user name
+        else:
+    # display user name
+    for  # iterate dic
+        print(f"{# name} ({#email})")
 
-for #iterate dic
-    print(f"{#name} ({#email})")
+
+main()
