26745324f344319c93f0571ee45208675059d6f0
yunsun <ys247071@gmail.com>
2024-03-11 15:25:34 +0800

    add email split feature

--- prac_05/4_emails.py
+++ prac_05/4_emails.py
@@ -6,8 +6,12 @@ Actual:    minutes
 
 def main():
     email_to_name = {}
-    email = input("Email: ")
+    email = input("Email: ")        # lindsay.ward@jcu.edu.au
     while email != '':
+        email_split_a = email.split('@')[0]
+        email_split_name = email_split_a.split('.')
+        name = email_split_name
+
 
         user_answer = input(f"Is your name {name}? (Y/n)").lower()
         if user_answer == "n":
