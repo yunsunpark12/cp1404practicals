873d45b2364d2fc6679b5296082d4be90498d987
yunsun <ys247071@gmail.com>
2024-03-11 15:29:21 +0800

    add get name from email func

--- prac_05/4_emails.py
+++ prac_05/4_emails.py
@@ -6,20 +6,23 @@ Actual:    minutes
 
 def main():
     email_to_name = {}
-    email = input("Email: ")        # lindsay.ward@jcu.edu.au
+    email = input("Email: ")        # e.g.  lindsay.ward@jcu.edu.au
     while email != '':
-        email_split_a = email.split('@')[0]
-        email_split_name = email_split_a.split('.')
-        name = email_split_name
-
-
-        user_answer = input(f"Is your name {name}? (Y/n)").lower()
-        if user_answer == "n":
+        name = get_name_from_email(email)
+        user_confirmation = input(f"Is your name {name}? (Y/n)").lower()
+        if user_confirmation == "n":
         # get rid of number
         else:
     # display user name
     for  # iterate dic
         print(f"{# name} ({#email})")
+
+
+def get_name_from_email(email):
+    email_splitted_by_a = email.split('@')[0]
+    name_in_email = email_splitted_by_a.split('.')
+    name =.join(name_in_email).title()
+    return name
 
 
 main()
