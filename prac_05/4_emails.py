25370c4dd192125fccc24ce3e34b73a52de7a4f3
yunsun <ys247071@gmail.com>
2024-03-11 15:17:30 +0800

    code draft

---
+++ prac_05/4_emails.py
@@ -0,0 +1,15 @@
+"""
+Estimate: 40 minutes
+Actual:    minutes
+"""
+
+email_to_name = {}
+while user_answer != '':
+    user_answer = input(f"Is your name {names}? (Y/n)").lower()
+    if user_answer == "n":
+        # get rid of number
+    else:
+        # display user name
+
+for #iterate dic
+    print(f"{#name} ({#email})")
