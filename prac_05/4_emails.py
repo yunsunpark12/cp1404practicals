1e86027c001949b75ff1fddae8133b7f862bc7cf
yunsun <ys247071@gmail.com>
2024-03-11 15:52:09 +0800

    add item func to dic print

--- prac_05/4_emails.py
+++ prac_05/4_emails.py
@@ -10,14 +10,14 @@ def main():
     while email != '':
         name = get_name_from_email(email)
         user_confirmation = input(f"Is your name {name}? (Y/n)").lower()
-        if user_confirmation == "y" or user_confirmation == '':
+        if user_confirmation == "y" or user_confirmation == ' ':
             # store both name & email to dictionary
             email_to_name[email] = name
         else:
             name = input("Name: ")
             email_to_name[email] = name
 
-    for email, name in email_to_name:
+    for email, name in email_to_name.items():
         print(f"{name} ({email})")
 
 
@@ -25,7 +25,7 @@ def get_name_from_email(email):
     """Get names from user email"""
     email_splitted_by_a = email.split('@')[0]
     name_in_email = email_splitted_by_a.split('.')
-    name =.join(name_in_email).title()
+    name =''.join(name_in_email).title()
     return name
 
 
