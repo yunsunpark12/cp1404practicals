69040a6b2821e775b1c578fede7feee94c588bad
yunsun <ys247071@gmail.com>
2024-03-11 15:44:25 +0800

    add docstrings, arrange while loop detail in main func

--- prac_05/4_emails.py
+++ prac_05/4_emails.py
@@ -1,24 +1,28 @@
 """
 Estimate: 40 minutes
-Actual:    minutes
+Actual:   30 minutes
 """
 
-
 def main():
+    """Get email dictionary to names and user confirmation """
     email_to_name = {}
     email = input("Email: ")        # e.g.  lindsay.ward@jcu.edu.au
     while email != '':
         name = get_name_from_email(email)
         user_confirmation = input(f"Is your name {name}? (Y/n)").lower()
-        if user_confirmation == "n":
-            # get rid of number and show name only
+        if user_confirmation == "y" or user_confirmation == '':
+            # store both name & email to dictionary
+            email_to_name[email] = name
         else:
-            # display user name and store both name&email to dictionary
-    for  # iterate dic
-        print(f"{# name} ({#email})")
+            name = input("Name: ")
+            email_to_name[email] = name
+
+    for email, name in email_to_name:
+        print(f"{name} ({email})")
 
 
 def get_name_from_email(email):
+    """Get names from user email"""
     email_splitted_by_a = email.split('@')[0]
     name_in_email = email_splitted_by_a.split('.')
     name =.join(name_in_email).title()
