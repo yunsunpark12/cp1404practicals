667c94b2be6dffbec207febb8b7f9f58fcc725e4
yunsun <ys247071@gmail.com>
2024-03-16 14:55:00 +0800

    add init, boolean, string, client test

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,6 +1,12 @@
 from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,11 +4,16 @@ Actual:    minutes
 """
 
 class ProgrammingLanguage:
-    def __init__(self, field=" ", typing=" ", reflection=" ", year=0):
+    def __init__(self, field, typing, reflection, year):
         """Initialise a Programming Language instance."""
 
         self.field = field
         self.typing = typing
         self.reflection = reflection
-        self.year = 0
+        self.year = year
 
+    def __str__(self):
+        return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
