dd25814926fd26a60740fb0cb65523ba92fb64d1
yunsun <ys247071@gmail.com>
2024-03-16 14:33:54 +0800

    define init

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""
+Estimate: 40 minutes
+Actual:    minutes
+"""
+
+class ProgrammingLanguage:
+    def __init__(self, field=" ", typing=" ", reflection=" ", year=0):
+        """Initialise a Programming Language instance."""
+
+        self.field = field
+        self.typing = typing
+        self.reflection = reflection
+        self.year = 0
+
