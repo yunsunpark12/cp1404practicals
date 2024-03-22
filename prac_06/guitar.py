edfddb96ae9f70710bb914b0771a0961fc4bc46d
yunsun <ys247071@gmail.com>
2024-03-22 15:32:47 +0800

    add age input

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,5 @@
+THIS_YEAR = 2024
+
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
@@ -6,4 +8,7 @@ class Guitar:
 
     def __str__(self):
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return THIS_YEAR - self.year
 
