c6b9a70ac8b78475b17fb2cb6b34964c59dd5c1b
yunsun <ys247071@gmail.com>
2024-04-08 13:51:34 +0800

    add class variable price_per_km and test

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -8,10 +8,11 @@ from prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -31,3 +32,4 @@ class Taxi(Car):
         distance_driven = super().drive(distance)
         self.current_fare_distance += distance_driven
         return distance_driven
+
