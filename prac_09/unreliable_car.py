bce70605f5e847aae7b0b35c544c5c9abddc0aa6
yunsun <ys247071@gmail.com>
2024-04-08 15:06:34 +0800

    edit attribute and test

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,13 +1,13 @@
-from prac_09.car import Car
 from random import randint
+from prac_09.car import Car
 
 class UnreliableCar(Car):
-    def __int__(self, reliability, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
-        random_number = randint(1, 100)
+        random_number = randint(0, 100)
         if random_number >= self.reliability:
             distance = 0
         driven_distance = super().drive(distance)
