319d37e1b123d68b880581cc34d4bd28bedc9dab
yunsun <ys247071@gmail.com>
2024-04-08 13:44:19 +0800

    start code

--- prac_09/car.py
+++ prac_09/car.py
@@ -1,17 +1,19 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, car_name=" ", fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: string, reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
-        self.car_name = car_name
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
 
     def __str__(self):
-        return f"{self.car_name}, fuel={self.fuel}, odometer={self.odometer}"
+        """Return a string representation of a Car object."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self.odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
